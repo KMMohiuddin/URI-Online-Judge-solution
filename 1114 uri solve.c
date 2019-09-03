2b8ef7db10cff743effcb8395def121871e1df08
Mohiuddin <52062238+Mohiuddin-diu@users.noreply.github.com>
2019-09-03 17:29:31 +0600

    Create 1114 uri solve.c

---
+++ 1114 uri solve.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,s,d;
+     for(d=1;d<=1001;d++)
+     {
+          scanf("%d",&a);
+    if (a==2002)
+    {
+        printf("Acesso Permitido\n");
+        break;
+    }
+    else
+    {
+      printf("Senha Invalida\n");
+
+     }
+
+     }
+     return 0;
+
+
+}
