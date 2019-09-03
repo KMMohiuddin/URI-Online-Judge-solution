df273de9796ec691da636858caea61e84053b5b7
Mohiuddin <52062238+Mohiuddin-diu@users.noreply.github.com>
2019-09-03 13:35:10 +0600

    Create 1070 uri solve.c

---
+++ 1070 uri solve.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,s;
+    scanf("%d",&s);
+    for(a=s;a<=s+11;a++)
+    {
+        if(a%2!=0)
+        {
+            printf("%d\n",a);
+        }
+
+    }
+}
+
+
